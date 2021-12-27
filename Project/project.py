e080399a2a028af47afc8f4494d736898ea7c152
enestelmaci <86835273+enestelmaci@users.noreply.github.com>
2021-12-27 13:08:44 +0300

    Add files via upload
    All functions are working properly except deblurring. Morever, the grayscale images are not allowed to use somefunctions.

--- Project/project.py
+++ Project/project.py
@@ -4,13 +4,26 @@ import tkinter.constants
 import tkinter.font as tkFont
 from tkinter.filedialog import askopenfile
 from PIL import ImageGrab, ImageFilter
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image, ImageEnhance
+from skimage.util import random_noise
 from tkinter import filedialog
 from tkinter import ttk
 
 import PIL
 import cv2
 import numpy as np
+
+
+def noise(img):
+    # Generate Gaussian noise
+    gauss = np.random.normal(0, 1, img.size)
+    if(len(img.shape)>2):
+        gauss = gauss.reshape(img.shape[0], img.shape[1], img.shape[2]).astype('uint8')
+    else:
+        gauss = gauss.reshape(img.shape[0], img.shape[1]).astype('uint8')
+    # Add the Gaussian noise to the image
+    img_gauss = cv2.add(img, gauss)
+    return img_gauss
 
 
 class App:
@@ -145,9 +158,9 @@ class App:
         Button5["font"] = ft
         Button5["fg"] = "#000000"
         Button5["justify"] = "center"
-        Button5["text"] = "Flip"
+        Button5["text"] = "Deblur"
         Button5.place(x=200, y=90, width=70, height=25)
-        # Button5["command"] = self.Button5_command
+        Button5["command"] = self.deblur_image
 
         Button6 = tk.Button(root)
         Button6["bg"] = "#efefef"
@@ -155,9 +168,9 @@ class App:
         Button6["font"] = ft
         Button6["fg"] = "#000000"
         Button6["justify"] = "center"
-        Button6["text"] = "Flip"
+        Button6["text"] = "adjust_brightness"
         Button6.place(x=200, y=120, width=70, height=25)
-        # Button6["command"] = self.Button6_command
+        Button6["command"] = self.adjust_brightness
 
         Button7 = tk.Button(root)
         Button7["bg"] = "#efefef"
@@ -165,9 +178,9 @@ class App:
         Button7["font"] = ft
         Button7["fg"] = "#000000"
         Button7["justify"] = "center"
-        Button7["text"] = "Flip"
+        Button7["text"] = "adjust_contrast"
         Button7.place(x=200, y=150, width=70, height=25)
-        # Button7["command"] = self.Button7_command
+        Button7["command"] = self.adjust_contrast
 
         Button8 = tk.Button(root)
         Button8["bg"] = "#efefef"
@@ -175,9 +188,29 @@ class App:
         Button8["font"] = ft
         Button8["fg"] = "#000000"
         Button8["justify"] = "center"
-        Button8["text"] = "Flip"
+        Button8["text"] = "add noise"
         Button8.place(x=200, y=180, width=70, height=25)
-        # Button8["command"] = self.Button8_command
+        Button8["command"] = self.add_noise
+
+        Button9 = tk.Button(root)
+        Button9["bg"] = "#efefef"
+        ft = tkFont.Font(family='Times', size=10)
+        Button9["font"] = ft
+        Button9["fg"] = "#000000"
+        Button9["justify"] = "center"
+        Button9["text"] = "adjust saturation"
+        Button9.place(x=100, y=180, width=70, height=25)
+        Button9["command"] = self.adjust_saturation
+
+        Edges = tk.Button(root)
+        Edges["bg"] = "#efefef"
+        ft = tkFont.Font(family='Times', size=10)
+        Edges["font"] = ft
+        Edges["fg"] = "#000000"
+        Edges["justify"] = "center"
+        Edges["text"] = "detect edges"
+        Edges.place(x=300, y=30, width=70, height=25)
+        Edges["command"] = self.detect_Edges
 
     # ************ EDIT ************
 
@@ -222,6 +255,31 @@ class App:
             lbl.grid(column=0, row=0)
             lbl.place(x=600, y=150, width=600, height=600)
             print("blured")
+        except:
+            from tkinter import messagebox
+
+            messagebox.showerror("Error", "U cannot use this process to this image.")
+
+    def deblur_image(self):
+        global input
+        global img_will_be_saved
+        global img_will_be_changed
+        try:
+
+            im = img_will_be_saved
+            enhancer = ImageEnhance.Sharpness(im)
+            im = enhancer.enhance(2)
+            img_will_be_saved = im
+            imlist = im.size
+            new_image = im.resize(
+                (int(imlist[0] / 3), int(imlist[1] / 3)) if (int(imlist[0]) > 600 or int(imlist[1]) > 600) else (
+                    imlist[0], imlist[1]))
+            img_will_be_changed = ImageTk.PhotoImage(new_image)
+
+            lbl = Label(image=img_will_be_changed)
+            lbl.grid(column=0, row=0)
+            lbl.place(x=600, y=150, width=600, height=600)
+            print("not working properly look at that later!!")
         except:
             from tkinter import messagebox
 
@@ -360,31 +418,52 @@ class App:
         global input
         global img_will_be_saved
         global img_will_be_changed
-        try:
 
-            im = img_will_be_saved
-            im = PIL.ImageOps.invert(im)
-            img_will_be_saved = im
-            imlist = im.size
-            new_image = im.resize(
-                (int(imlist[0] / 3), int(imlist[1] / 3)) if (int(imlist[0]) > 600 or int(imlist[1]) > 600) else (
-                    imlist[0], imlist[1]))
-            img_will_be_changed = ImageTk.PhotoImage(new_image)
 
-            lbl = Label(image=img_will_be_changed)
-            lbl.grid(column=0, row=0)
-            lbl.place(x=600, y=150, width=600, height=600)
-        except:
-            from tkinter import messagebox
+        im = img_will_be_saved
+        im = PIL.ImageOps.invert(im)
+        img_will_be_saved = im
+        imlist = im.size
+        new_image = im.resize(
+            (int(imlist[0] / 3), int(imlist[1] / 3)) if (int(imlist[0]) > 600 or int(imlist[1]) > 600) else (
+                imlist[0], imlist[1]))
+        img_will_be_changed = ImageTk.PhotoImage(new_image)
 
-            messagebox.showerror("Error", "U cannot use this process to this image.")
+        lbl = Label(image=img_will_be_changed)
+        lbl.grid(column=0, row=0)
+        lbl.place(x=600, y=150, width=600, height=600)
+
+
+        print("command")
+    def detect_Edges(self):
+        global input
+        global img_will_be_saved
+        global img_will_be_changed
+
+
+        im = img_will_be_saved
+        im_array=np.array(im)
+        gray = cv2.cvtColor(im_array, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray, threshold1=30, threshold2=100)
+        im=Image.fromarray(edges)
+
+        img_will_be_saved = im
+        imlist = im.size
+        new_image = im.resize(
+            (int(imlist[0] / 3), int(imlist[1] / 3)) if (int(imlist[0]) > 600 or int(imlist[1]) > 600) else (
+                imlist[0], imlist[1]))
+        img_will_be_changed = ImageTk.PhotoImage(new_image)
+
+        lbl = Label(image=img_will_be_changed)
+        lbl.grid(column=0, row=0)
+        lbl.place(x=600, y=150, width=600, height=600)
+
         print("command")
 
     def ChangeRGB_command(self):
         global input
         global img_will_be_saved
         global img_will_be_changed
-
 
         im = img_will_be_saved
 
@@ -438,6 +517,162 @@ class App:
         imlist = im.size
         new_image = im.resize(
             (int(imlist[0] / 3), int(imlist[1] / 3)) if (int(imlist[0]) > 600 or int(imlist[1]) > 600) else (
+                imlist[0], imlist[1]))
+        img_will_be_changed = ImageTk.PhotoImage(new_image)
+
+        lbl = Label(image=img_will_be_changed)
+        lbl.grid(column=0, row=0)
+        lbl.place(x=600, y=150, width=600, height=600)
+
+    def adjust_brightness(self):
+        global input
+        global img_will_be_saved
+        global img_will_be_changed
+
+        im = img_will_be_saved
+
+        root3 = Tk()
+        root3.title("brightness")
+        root3.geometry("400x400")
+
+        name_var = tk.StringVar()
+        red = tk.Text(root3, height=1)
+        red.pack()
+
+        def getTextInput():
+            global redresult
+            redresult = float(red.get("1.0", tk.END + "-1c"))
+
+        l = Label(root3, text="Type a value to adjust brightness of image.")
+        l.pack()
+        b2 = Button(root3, text='finish', command=root3.destroy)
+        b2.pack(side='bottom')
+        b = Button(root3, text='apply', command=getTextInput)
+        b.pack(side='bottom')
+
+        root3.wait_window(b)
+
+        factor = redresult
+        img_brightness_obj = ImageEnhance.Brightness(im)
+        im = img_brightness_obj.enhance(factor)
+
+        img_will_be_saved = im
+        imlist = im.size
+        new_image = im.resize(
+            (int(imlist[0] / 3), int(imlist[1] / 3)) if (int(imlist[0]) > 600 or int(imlist[1]) > 600) else (
+                imlist[0], imlist[1]))
+        img_will_be_changed = ImageTk.PhotoImage(new_image)
+
+        lbl = Label(image=img_will_be_changed)
+        lbl.grid(column=0, row=0)
+        lbl.place(x=600, y=150, width=600, height=600)
+
+    def adjust_contrast(self):
+        global input
+        global img_will_be_saved
+        global img_will_be_changed
+
+        im = img_will_be_saved
+
+        root3 = Tk()
+        root3.title("contrast")
+        root3.geometry("400x400")
+
+        name_var = tk.StringVar()
+        red = tk.Text(root3, height=1)
+        red.pack()
+
+        def getTextInput():
+            global redresult
+            redresult = float(red.get("1.0", tk.END + "-1c"))
+
+        l = Label(root3, text="Type a value to adjust contrast of image.")
+        l.pack()
+        b2 = Button(root3, text='finish', command=root3.destroy)
+        b2.pack(side='bottom')
+        b = Button(root3, text='apply', command=getTextInput)
+        b.pack(side='bottom')
+
+        root3.wait_window(b)
+
+        factor = redresult
+        img_brightness_obj = ImageEnhance.Contrast(im)
+        im = img_brightness_obj.enhance(factor)
+
+        img_will_be_saved = im
+        imlist = im.size
+        print(im.size)
+        new_image = im.resize(
+            (int(imlist[0] / 3), int(imlist[1] / 3)) if (int(imlist[0]) > 650 or int(imlist[1]) > 650) else (
+                imlist[0], imlist[1]))
+        img_will_be_changed = ImageTk.PhotoImage(new_image)
+
+        lbl = Label(image=img_will_be_changed)
+        lbl.grid(column=0, row=0)
+        lbl.place(x=600, y=150, width=600, height=600)
+
+    def adjust_saturation(self):
+        global input
+        global img_will_be_saved
+        global img_will_be_changed
+
+        im = img_will_be_saved
+
+        img_enhanced=ImageEnhance.Color(im)
+        root3 = Tk()
+        root3.title("contrast")
+        root3.geometry("400x400")
+
+        name_var = tk.StringVar()
+        red = tk.Text(root3, height=1)
+        red.pack()
+
+        def getTextInput():
+            global redresult
+            redresult = float(red.get("1.0", tk.END + "-1c"))
+
+        l = Label(root3, text="Type a value to adjust contrast of image.")
+        l.pack()
+        b2 = Button(root3, text='finish', command=root3.destroy)
+        b2.pack(side='bottom')
+        b = Button(root3, text='apply', command=getTextInput)
+        b.pack(side='bottom')
+
+        root3.wait_window(b)
+
+        factor = redresult
+        im=img_enhanced.enhance(factor)
+
+
+
+
+
+        img_will_be_saved = im
+        imlist = im.size
+        print(im.size)
+        new_image = im.resize(
+            (int(imlist[0] / 3), int(imlist[1] / 3)) if (int(imlist[0]) > 650 or int(imlist[1]) > 650) else (
+                imlist[0], imlist[1]))
+        img_will_be_changed = ImageTk.PhotoImage(new_image)
+
+        lbl = Label(image=img_will_be_changed)
+        lbl.grid(column=0, row=0)
+        lbl.place(x=600, y=150, width=600, height=600)
+
+    def add_noise(self):
+        global input
+        global img_will_be_saved
+        global img_will_be_changed
+
+        im = img_will_be_saved
+        np_im = np.array(im)
+        np_im = noise(np_im)
+        im = Image.fromarray(np_im)
+        img_will_be_saved = im
+        imlist = im.size
+
+        new_image = im.resize(
+            (int(imlist[0] / 3), int(imlist[1] / 3)) if (int(imlist[0]) > 650 or int(imlist[1]) > 650) else (
                 imlist[0], imlist[1]))
         img_will_be_changed = ImageTk.PhotoImage(new_image)
 
